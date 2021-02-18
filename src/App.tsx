08b43e53510cfb9d547a06cf160bdf7ad863efa6
josipfrljic94 <josipfrljic77@gmail.com>
2021-02-18 15:03:15 +0100

    sc

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {Provider} from "react-redux";
+import {store,} from "./state";
+import RepositoriesList from "./components/RepositoriesList";
+import Container from "./Style/Container";
+import Jobs from "./components/Jobs";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+        <Container/>
+        <RepositoriesList/>
+        <Jobs/>
+    </Provider>
   );
 }
 
